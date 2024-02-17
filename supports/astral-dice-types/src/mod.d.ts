75d0d577ea1c9f7d10b777856e607b7f51496d3b
umajho <umajho@proton.me>
2024-02-17 21:53:53 +0800

    refactor(astral-dice-types): 调整类型声明，以能单独使用 Lib 的类型

--- supports/astral-dice-types/src/mod.d.ts
+++ supports/astral-dice-types/src/mod.d.ts
@@ -1,34 +1,36 @@
-namespace Lib {
+interface AstralDiceAPI {
   /**
    * 返回是否为群聊。
    */
-  function groupChat(): boolean;
+  groupChat(): boolean;
   /**
    * 获取群号。
    */
-  function getGroup(): String;
+  getGroup(): String;
   /**
    * 回复。
    */
-  function reply(msg: string): string;
+  reply(msg: string): string;
   /**
    * 获取变量。
    */
-  function getValue(key: string): String;
+  getValue(key: string): String;
   /**
    * 持久化 `key` 对应的变量。
    */
-  function setAsSolidValue(key: string);
+  setAsSolidValue(key: string);
   /**
    * 设置变量。
    */
-  function setValue(key: string, value: string);
+  setValue(key: string, value: string);
   /**
    * 目前只能确定把第二个参数设为 “0” 能删除掉变量。
    */
-  function setValueExpireTime(key: string, zero: 0);
+  setValueExpireTime(key: string, zero: 0);
   /**
    * 获取配置（同一 `auto` 下名为 key 的属性）。
    */
-  function getConfig(key: string): String;
+  getConfig(key: string): String;
 }
+
+declare const Lib: AstralDiceAPI;
